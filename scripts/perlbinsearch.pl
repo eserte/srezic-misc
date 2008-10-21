2d4d7bb73e13b57e4aba2239d4d91ec6385f71db
Slaven Rezic <slaven@rezic.de>
2008-10-21 20:36:07 +0000

    * binsearch and CPAN.pm support

--- scripts/perlbinsearch.pl
+++ scripts/perlbinsearch.pl
@@ -2,7 +2,7 @@
 # -*- perl -*-
 
 #
-# $Id: perlbinsearch.pl,v 1.5 2008/10/21 20:36:03 eserte Exp $
+# $Id: perlbinsearch.pl,v 1.6 2008/10/21 20:36:07 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2008 Slaven Rezic. All rights reserved.
@@ -27,12 +27,26 @@ my $perldir = cwd;
 
 #my $distribution = "/usr/local/src/CPAN/build/autorequire-0.08-A3c4FR";
 #my $distribution = "/usr/local/src/CPAN/build/Attribute-Tie-0.01-HxCk5z";
-my $distribution = "/usr/local/src/CPAN/build/Apache-Admin-Config-0.94-nzBcpg";
+#my $distribution = "/usr/local/src/CPAN/build/Apache-Admin-Config-0.94-nzBcpg";
+#my $distribution = "/usr/local/src/CPAN/build/MPEG-Audio-Frame-0.09-avy6LE";
+#my $distribution = "/usr/local/src/CPAN/build/Acme-Oil-0.1-IUxiWf";
+my $distribution;
 
+#my $cpanmod;
+my $cpanmod = "ex::lib::zip";
+
+my $checkcmd;
 #my $checkcmd = "env PERL5LIB=$perldir/lib make test";
 #my $checkcmd = "env PERL5LIB=$perldir/lib $perldir/perl -Mblib t/03_autodynaload_hook.t";
 #my $checkcmd = "env PERL5LIB=$perldir/lib $perldir/perl -Mblib t/02-array.t";
-my $checkcmd = "env PERL5LIB=$perldir/lib make test TEST_FILES=t/value.t";
+#my $checkcmd = "env PERL5LIB=$perldir/lib make test TEST_FILES=t/value.t";
+#my $checkcmd = "env PERL5LIB=$perldir/lib make test TEST_FILES=t/04-tie.t";
+#my $checkcmd = "env PERL5LIB=$perldir/lib make test TEST_FILES=t/02-scalar.t";
+
+if ($distribution && $cpanmod) {
+    warn "What do you want: distribution directory or CPAN mod?";
+    exit 258;
+}
 
 $SIG{__WARN__} = sub {
     print @_;
@@ -50,19 +64,30 @@ RUN: {
     system('./configure.gnu', '-Dcc=ccache cc') == 0 or last RUN;
     warn "make";
     system('make', '-j4') == 0 or last RUN;
-    warn "chdir to cpan dist";
-    chdir $distribution or do {
-	$err = 256;
-	warn "Cannot chdir to $distribution: $!";
-	last RUN;
-    };
-    warn "perl Makefile.PL";
-    system("$perldir/perl", "-I$perldir/lib", "Makefile.PL") == 0 or last RUN;
-    warn "make $distribution";
-    system("make") == 0 or last RUN;
-    warn "check command: $checkcmd";
-    system($checkcmd);
-    $err = $?==0 ? 0 : 1;
+    if ($cpanmod) {
+	warn "Testing $cpanmod with CPAN.pm";
+	my $cmd = "env PERL5LIB=$perldir/lib $perldir/perl -MCPAN -e '" .
+	    q{$cpanmod = "} .
+		$cpanmod .
+		    q{"; $CPAN::Config->{test_report} = 0; test($cpanmod); $success = eval { not CPAN::Shell->expand("Module", "$cpanmod")->distribution->{make_test}->failed }; exit($success ? 0 : 1)';};
+	print STDERR $cmd, "\n";
+	system($cmd);
+	$err = $?==0 ? 0 : 1;
+    } else {
+	warn "chdir to cpan dist";
+	chdir $distribution or do {
+	    $err = 256;
+	    warn "Cannot chdir to $distribution: $!";
+	    last RUN;
+	};
+	warn "perl Makefile.PL";
+	system("$perldir/perl", "-I$perldir/lib", "Makefile.PL") == 0 or last RUN;
+	warn "make $distribution";
+	system("make") == 0 or last RUN;
+	warn "check command: $checkcmd";
+	system($checkcmd);
+	$err = $?==0 ? 0 : 1;
+    }
     warn "error code is $err";
 }
 warn "final cleanup";
