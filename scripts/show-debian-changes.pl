057209c42812d206748b52d84bbb944d52cf47d8
Slaven Rezic <slaven@rezic.de>
2014-08-23 10:56:40 +0200

    additional parsing using dpkg-parsechangelog
    This is now the default if dpkg-parsechangelog is installed. Otherwise
    use the -method option.

--- scripts/show-debian-changes.pl
+++ scripts/show-debian-changes.pl
@@ -13,6 +13,17 @@
 #
 
 use strict;
+use Getopt::Long;
+
+my $method;
+GetOptions("method=s" => \$method)
+    or die "usage?";
+
+if (!$method && is_in_path('dpkg-parsechangelog')) {
+    $method = 'dpkg-parsechangelog';
+} else {
+    $method = 'manually';
+}
 
 my $dpkg_log = shift || "/var/log/dpkg.log";
 
@@ -22,43 +33,105 @@ while(<$fh>) {
 	print "-"x70, "\n";
 	print $_;
 	my $changelog = "/usr/share/doc/$package/changelog.Debian.gz";
-	open my $changelog_fh, "-|", "zcat", $changelog
-	    or do {
-		print "ERROR: Cannot read $changelog, skipping this package (error: $@)\n";
-		next;
-	    };
-	my $changelog_contents = '';
-	my $state = 'search_newer_version';
-	while(<$changelog_fh>) {
-	    if ($state eq 'search_newer_version') {
-		if (m{^\Q$package ($to_version)}) {
-		    $changelog_contents .= $_;
-		    $state = 'search_older_version';
-		}
-	    } elsif ($state eq 'search_older_version') {
-		if (m{^\Q$package ($from_version)}) {
-		    $state = 'finished';
-		    # don't "last" out of the loop, consume the changelog till the end to avoid SIGPIPE errors
-		} else {
-		    $changelog_contents .= $_;
+	if (!-f $changelog) {
+	    print "ERROR: the changelog file '$changelog' does not exist, skipping this package\n";
+	    next;
+	}
+
+	my $parse_manually = sub {
+	    open my $changelog_fh, "-|", "zcat", $changelog
+		or do {
+		    print "ERROR: Cannot read $changelog, skipping this package (error: $@)\n";
+		    return;
+		};
+	    my $changelog_contents = '';
+	    my $state = 'search_newer_version';
+	    while (<$changelog_fh>) {
+		if ($state eq 'search_newer_version') {
+		    if (m{^\S+ \Q($to_version)}) {
+			$changelog_contents .= $_;
+			$state = 'search_older_version';
+		    }
+		} elsif ($state eq 'search_older_version') {
+		    if (m{^\S+ \Q($from_version)}) {
+			$state = 'finished';
+			# don't "last" out of the loop, consume the changelog till the end to avoid SIGPIPE errors
+		    } else {
+			$changelog_contents .= $_;
+		    }
 		}
 	    }
-	}
-	close $changelog_fh
-	    or do {
-		print "ERROR: problem reading from $changelog (error: $!)\n";
-		next;
-	    };
+	    close $changelog_fh
+		or do {
+		    print "ERROR: problem reading from $changelog (error: $!)\n";
+		    return;
+		};
 
-	if ($state eq 'search_newer_version') {
-	    print "ERROR: cannot find newer version '$to_version' in $changelog.\n";
-	} elsif ($state eq 'search_older_version') {
-	    print "ERROR: cannot find older version '$from_version' in $changelog.\n";
+	    if ($state eq 'search_newer_version') {
+		print "ERROR: cannot find newer version '$to_version' in $changelog.\n";
+	    } elsif ($state eq 'search_older_version') {
+		print "ERROR: cannot find older version '$from_version' in $changelog.\n";
+	    } else {
+		print $changelog_contents;
+	    }
+	};
+
+	my $parse_with_dpkg_parsechangelog = sub {
+	    open my $changelog_fh, "zcat $changelog | dpkg-parsechangelog -l- --since '$from_version' --to '$to_version' 2>&1 |"
+		or do {
+		    print "ERROR: problem parsing $changelog with dpkg-parsechangelog.\n";
+		    return;
+		};
+	    while(<$changelog_fh>) {
+		print $_;
+	    }
+	};
+
+	if ($method && $method eq 'dpkg-parsechangelog') {
+	    $parse_with_dpkg_parsechangelog->();
+	} elsif (!$method || $method eq 'manually') {
+	    $parse_manually->();
 	} else {
-	    print $changelog_contents;
+	    die "FATAL: Invalid method '$method'";
 	}
     }
 }
+
+# REPO BEGIN
+# REPO NAME is_in_path /home/e/eserte/src/srezic-repository 
+# REPO MD5 e18e6687a056e4a3cbcea4496aaaa1db
+sub is_in_path {
+    my($prog) = @_;
+    require File::Spec;
+    if (File::Spec->file_name_is_absolute($prog)) {
+	if ($^O eq 'MSWin32') {
+	    return $prog       if (-f $prog && -x $prog);
+	    return "$prog.bat" if (-f "$prog.bat" && -x "$prog.bat");
+	    return "$prog.com" if (-f "$prog.com" && -x "$prog.com");
+	    return "$prog.exe" if (-f "$prog.exe" && -x "$prog.exe");
+	    return "$prog.cmd" if (-f "$prog.cmd" && -x "$prog.cmd");
+	} else {
+	    return $prog if -f $prog and -x $prog;
+	}
+    }
+    require Config;
+    %Config::Config = %Config::Config if 0; # cease -w
+    my $sep = $Config::Config{'path_sep'} || ':';
+    foreach (split(/$sep/o, $ENV{PATH})) {
+	if ($^O eq 'MSWin32') {
+	    # maybe use $ENV{PATHEXT} like maybe_command in ExtUtils/MM_Win32.pm?
+	    return "$_\\$prog"     if (-f "$_\\$prog" && -x "$_\\$prog");
+	    return "$_\\$prog.bat" if (-f "$_\\$prog.bat" && -x "$_\\$prog.bat");
+	    return "$_\\$prog.com" if (-f "$_\\$prog.com" && -x "$_\\$prog.com");
+	    return "$_\\$prog.exe" if (-f "$_\\$prog.exe" && -x "$_\\$prog.exe");
+	    return "$_\\$prog.cmd" if (-f "$_\\$prog.cmd" && -x "$_\\$prog.cmd");
+	} else {
+	    return "$_/$prog" if (-x "$_/$prog" && !-d "$_/$prog");
+	}
+    }
+    undef;
+}
+# REPO END
 
 __END__
 
@@ -85,6 +158,6 @@ Slaven Rezic
 
 =head1 SEE ALSO
 
-L<apt-listchanges(1)>.
+L<dpkg-parsechangelog(1)>, L<apt-listchanges(1)>.
 
 =cut
