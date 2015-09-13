86dbb2918618e5d737230c439f9f4503ca92c2d8
Slaven Rezic <slaven@rezic.de>
2015-09-13 20:39:48 +0200

    another regexp for detecting system perl usage

--- scripts/ctr_good_or_invalid.pl
+++ scripts/ctr_good_or_invalid.pl
@@ -508,6 +508,7 @@ sub parse_test_report {
 		    $add_analysis_tag->('prereq test');
 		} elsif (   # this should come before the generic 'prereq fail' test
 			    m{^(?:#\s+Error:\s+)?Can't locate \S+ in \@INC .*\(\@INC contains.* /etc/perl} # Debian version
+			 || m{^\s*or make that module available in \@INC \(\@INC contains.* /etc/perl} # base class error, Debian version
 			 || m{^(?:#\s+Error:\s+)?Can't locate \S+ in \@INC .*\(\@INC contains.* /usr/local/lib/perl5/5.\d+/BSDPAN} # FreeBSD version, old
 			 || m{^(?:#\s+Error:\s+)?Can't locate \S+ in \@INC .*\(\@INC contains.* /usr/local/lib/perl5/site_perl/mach/} # FreeBSD version, new
 			 || m{Undefined symbol ".*" at /usr/local/lib/perl5/5.\d+/mach/}, # wrong linking, FreeBSD version, new
