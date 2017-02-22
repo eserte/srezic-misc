0c1c0777c2598caf54806a9afda4323e0d79964b
Slaven Rezic <slaven@rezic.de>
2017-02-22 07:17:20 +0100

    guess the next beforemaintrelease pair

--- scripts/ctr_good_or_invalid.pl
+++ scripts/ctr_good_or_invalid.pl
@@ -40,6 +40,7 @@ use constant USE_BETA_MATRIX => 0;
 
 my @current_beforemaintrelease_pairs = (
 					'5.24.0:5.25.9',
+					'5.24.0:5.25.10',
  					'5.24.0:5.24.1',
  					'5.22.2:5.22.3',
 				       );
