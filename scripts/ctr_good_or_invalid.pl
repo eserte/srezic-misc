520007886b6a98ce26c5f1b275385f5388c81abd
Slaven Rezic <slaven@rezic.de>
2017-03-02 20:32:11 +0100

    regexp for c++ compile errors

--- scripts/ctr_good_or_invalid.pl
+++ scripts/ctr_good_or_invalid.pl
@@ -720,6 +720,10 @@ sub parse_test_report {
 			) {
 		    $add_analysis_tag->('other c compiler error');
 		} elsif (
+			 m{^/usr/include/c\+\+/[^:]+:\d+:\d+: fatal error: } # g++
+			) {
+		    $add_analysis_tag->('c++ compile error');
+		} elsif (
 			 /^\s*(#\s+Error:\s+)?Can't load '.*?\.so' for module .*: Undefined symbol ".*?" $at_source_qr/ || # freebsd variant
 			 /^\s*(#\s+Error:\s+)?Can't load '.*?\.so' for module .*: .*?\.so: undefined symbol: \S+ $at_source_qr/ || # linux variant
 			 /^\s*(#\s+Error:\s+)?Can't load '.*?\.so' for module .*: .*?\.so(?:\.\d+)?: perl: fatal: relocation error: .*: referenced symbol not found $at_source_qr/ # solaris variant
