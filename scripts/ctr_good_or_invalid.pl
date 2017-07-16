c62e79dac02fb7b64e1ae53fce5f69b9a3e22dcb
Slaven Rezic <slaven@rezic.de>
2017-07-16 13:15:01 +0200

    guess the next beforemaintrelease pairs

--- scripts/ctr_good_or_invalid.pl
+++ scripts/ctr_good_or_invalid.pl
@@ -43,8 +43,8 @@ my @current_beforemaintrelease_pairs = (
  					'5.24.0:5.24.1',
  					'5.22.2:5.22.3',
 					'5.26.0:5.27.1',
-					'5.22.3:5.22.4 RC1',
-					'5.24.1:5.24.2 RC1',
+					'5.22.3:5.22.4',
+					'5.24.1:5.24.2',
 				       );
 
 # Patterns for report analysis
