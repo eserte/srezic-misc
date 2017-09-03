000a46686023209a7d7310f8bfa34559335f683d
Slaven Rezic <slaven@rezic.de>
2017-09-03 20:02:55 +0200

    helper script for Windows smokers

---
+++ scripts/capture-tiny-watcher.pl
@@ -0,0 +1,162 @@
+#!/usr/bin/perl -w
+# -*- perl -*-
+
+#
+# Author: Slaven Rezic
+#
+# Copyright (C) 2017 Slaven Rezic. All rights reserved.
+# This program is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+# Mail: slaven@rezic.de
+# WWW:  http://www.rezic.de/eserte/
+#
+
+use strict;
+use Getopt::Long;
+use P9Y::ProcessTable;
+
+sub y_or_n (;$);
+
+GetOptions(
+	   "batch"   => \my $batch,
+	   "dry-run" => \my $dry_run,
+	   "debug"   => \my $debug,
+	  )
+    or die "usage: $0 [--batch] [--dry-run] [--debug]\n";
+
+while () {
+    my %pid_to_childpid;
+    my %candidate_ppid;
+
+    my @processes = P9Y::ProcessTable->table;
+
+    for my $process (@processes) {
+	my $pid = $process->pid;
+	my $ppid = $process->ppid;
+	if ($ppid) {
+	    push @{ $pid_to_childpid{$ppid} }, $pid;
+	}
+
+	no warnings 'uninitialized';
+	if (
+	    $process->exe =~ m{perl\.exe$} &&
+	    $process->cmdline =~ m{use Fcntl.*sysopen.*syswrite.*AppData\\Local\\Temp}s
+	   ) {
+	    my $duration = time - $process->start;
+	    if ($duration >= 2) {
+		push @{ $candidate_ppid{$ppid} }, $process;
+	    }
+	}
+    }
+
+    for my $ppid (keys %candidate_ppid) {
+	if (
+	    join(",", sort                 @{ $pid_to_childpid{$ppid} || [] }) eq
+	    join(",", sort map { $_->pid } @{ $candidate_ppid{$ppid} })
+	   ) {
+	    print STDERR "Candidates to kill:\n";
+	    for my $process (@{ $candidate_ppid{$ppid} }) {
+		print STDERR "  " . $process->pid . " (duration: " . (time-$process->start) . "s)\n";
+	    }
+
+	    my $do_kill;
+	    if (!$batch) {
+		print STDERR "Kill processes? (y/n) ";
+		if (y_or_n) {
+		    $do_kill = 1;
+		}
+	    } else {
+		$do_kill = 1;
+	    }
+
+	    if ($do_kill) {
+		for my $process (@{ $candidate_ppid{$ppid} }) {
+		    print STDERR "Kill process " . $process->pid . "... ";
+		    if ($dry_run) {
+			print STDERR "(dry-run)\n";
+		    } else {
+			$process->kill(9);
+			print STDERR "done\n";
+		    }
+		}
+	    }
+	} else {
+	    if ($debug) {
+		print STDERR "$ppid is probably still active, do not kill candidates...";
+	    }
+	}
+    }
+    sleep 1;
+}
+
+# REPO BEGIN
+# REPO NAME y_or_n /home/eserte/src/srezic-repository 
+# REPO MD5 146cfcf8f954555fe0117a55b0ddc9b1
+
+=head2 y_or_n
+
+Accept user input. Return true on 'y', return false on 'n', otherwise
+ask again.
+
+A default may be supplied as an optional argument:
+
+    y_or_n 'y';
+    y_or_n 'n';
+
+=cut
+
+sub y_or_n (;$) {
+    my $default = shift;
+    while () {
+        chomp(my $yn = <STDIN>);
+	if ($yn eq '' && defined $default) {
+	    $yn = $default;
+	}
+        if (lc $yn eq 'y') {
+            return 1;
+        } elsif (lc $yn eq 'n') {
+	    return 0;
+        } else {
+            print STDERR "Please answer y or n: ";
+        }
+    }
+}
+# REPO END
+
+__END__
+
+=head1 NAME
+
+capture-tiny-watcher.pl - kill hanging Capture::Tiny processes
+
+=head1 SYNOPSIS
+
+    perl capture-tiny-watcher.pl [--batch] [--debug] [--dry-run]
+
+=head1 DESCRIPTION
+
+Watch for possibly hanging Capture::Tiny processes on Windows systems,
+and kill them automatically (in C<--batch> mode) or interactively
+(default if C<--batch> is not specified) or just show what would be
+killed (in C<--dry-run> mode).
+
+Problem was described in
+
+=over
+
+=item * L<https://github.com/cpan-testers/CPAN-Reporter/issues/28>
+
+=item * L<http://blogs.perl.org/users/michael/2012/12/smoke-testing-on-windows.html>
+
+=back
+
+=head1 AUTHOR
+
+Slaven Rezic <srezic@cpan.org>
+
+=head1 SEE ALSO
+
+L<PY9::ProcessTable>.
+
+=cut
