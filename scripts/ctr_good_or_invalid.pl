28b763767cf3386dade3d633fc3f4d9f05345b18
Slaven Rezic <slaven@rezic.de>
2018-12-18 22:55:48 +0100

    update beforemaintrelease pair

--- scripts/ctr_good_or_invalid.pl
+++ scripts/ctr_good_or_invalid.pl
@@ -39,7 +39,7 @@ sub sort_by_example ($@);
 use constant USE_BETA_MATRIX => 0;
 
 my @current_beforemaintrelease_pairs = ( # remember: put a space before "RC", not a dash
-					'5.28.1:5.29.5',
+					'5.28.1:5.29.6',
 					'5.28.0:5.28.1',
 					'5.26.3:5.28.1',
 					'5.26.2:5.26.3',
