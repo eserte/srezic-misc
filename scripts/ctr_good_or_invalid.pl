1029634ec695783675ca9d9c75d1a5b5df036b5d
Slaven Rezic <slaven@rezic.de>
2019-12-21 21:47:02 +0100

    fix beforemaintrelease pair

--- scripts/ctr_good_or_invalid.pl
+++ scripts/ctr_good_or_invalid.pl
@@ -40,7 +40,7 @@ use constant USE_BETA_MATRIX => 0;
 
 my @current_beforemaintrelease_pairs = ( # remember: put a space before "RC", not a dash
 					{ pair => '5.30.0:5.31.7',     important => 0 },
-					{ pair => '5.31.5:5.31.7',     important => 0 },
+					{ pair => '5.31.6:5.31.7',     important => 0 },
 					{ pair => '5.30.0:5.30.1',     important => 0 },
 					{ pair => '5.28.2:5.30.0',     important => 0 },
 					{ pair => '5.28.1:5.28.2',     important => 0 },
