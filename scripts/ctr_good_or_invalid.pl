57d27197df9e9dd61066e6a74637007f664f740e
Slaven Rezic <slaven@rezic.de>
2020-06-28 19:15:17 +0200

    another analysis rx; update beforemaintrelease pairs

--- scripts/ctr_good_or_invalid.pl
+++ scripts/ctr_good_or_invalid.pl
@@ -39,9 +39,9 @@ sub sort_by_example ($@);
 use constant USE_BETA_MATRIX => 0;
 
 my @current_beforemaintrelease_pairs = ( # remember: put a space before "RC", not a dash
-					{ pair => '5.30.2:5.31.11',    important => 1 },
+					{ pair => '5.30.3:5.32.0',     important => 0 },
 					{ pair => '5.30.2:5.30.3',     important => 0 },
-					{ pair => '5.31.10:5.31.11',   important => 0 },
+					#{ pair => '5.31.11:5.32.0 RC1', important => 0 },
 					{ pair => '5.28.2:5.30.3',     important => 0 },
 					{ pair => '5.28.1:5.28.2',     important => 0 },
 					{ pair => '5.26.3:5.28.2',     important => 0 },
@@ -1195,7 +1195,8 @@ sub parse_test_report {
 		    $add_analysis_tag->('!!!no space left on device!!!');
 		} elsif (
 			 m{Could not execute .* open3: exec of .* failed: Argument list too long at .*TAP/Parser/Iterator/Process.pm} ||
-			 m{Can't exec ".*": Argument list too long at }
+			 m{Can't exec ".*": Argument list too long at } ||
+		         m{open3: exec of .* failed: Argument list too long at }
 			) {
 		    $add_analysis_tag->('!!!cmdline limits exceeded!!!');
 		} elsif (
