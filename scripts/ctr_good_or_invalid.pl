d6aec61389c6939397b27992d7909a7a1e482e1d
Slaven Rezic <slaven@rezic.de>
2026-04-07 16:15:33 +0200

    new archname match

--- scripts/ctr_good_or_invalid.pl
+++ scripts/ctr_good_or_invalid.pl
@@ -2474,7 +2474,7 @@ sub rough_pv_os_analysis {
 		    $arch_os_version = 'noble'; # 'Ubuntu 24.04?';
 		} elsif ($entry->{archname} eq 'aarch64-linux-gnu-thread-multi 6.1.0') {
 		    $arch_os_version = 'noble'; # 'Ubuntu 24.04? (system perl)';
-		} elsif ($entry->{archname} =~ m{ 6.1.0-(25|26|28|31|33|37|42)-}) {
+		} elsif ($entry->{archname} =~ m{ 6.1.0-(25|26|28|31|33|37|42|44)-}) {
 		    $arch_os_version = 'bookworm'; # 'Debian/bookworm?';
 		} elsif ($entry->{archname} =~ m{ 6\.1\.0$}) {
 		    $arch_os_version = 'trixie'; # 'Debian/trixie?';
